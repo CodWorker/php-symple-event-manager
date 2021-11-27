24491d23e8b1c5f165749048b7200bfd719bf8fa
CodWorker <thismyemailnew@gmail.com>
2021-11-27 19:15:09 +0200

    Create symple event manager

--- index.php
+++ index.php
@@ -4,4 +4,30 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Coder\TestWork1\EventManager;
 
-(new EventManager())->go();
+// test cases
+$firstFunc = function() {
+    echo "called first\n";
+};
+
+$secondFunc = function() {
+    echo "called second\n";
+};
+
+$thirdFunc = function() {
+    echo "called third\n";
+};
+
+$em = new EventManager();
+$em->addEventListener("type_one", $firstFunc);
+$em->addEventListener("type_two", $secondFunc);
+$em->addEventListener("type_two", $thirdFunc);
+
+
+$em->fireEvent("type_one");
+$em->fireEvent("type_two");
+
+
+// expected output:
+// called first
+// called second
+// called third
--- src/EventManager.php
+++ src/EventManager.php
@@ -2,17 +2,49 @@
 
 namespace Coder\TestWork1;
 
+/**
+ * Class EventManager
+ * 
+ * Basic usage:
+ * 
+ * $em = new EventManager();
+ * $em->addEventListener("type_one", $firstFunc);
+ * $em->addEventListener("type_two", $secondFunc);
+ * $em->addEventListener("type_two", $thirdFunc);
+ *
+ *
+ * $em->fireEvent("type_one");
+ * $em->fireEvent("type_two");
+ */
 class EventManager implements IEventManager 
 {
+    /**
+     * 
+     * [
+     *     "type_one" => [
+     *         [0] => callable $callback,
+     *         [1] => callable $callback
+     *         ...
+     *     ],
+     * 
+     * 
+     * ]
+     *
+     * @var array
+     */
+    private $events = [];
+    
     public function addEventListener($type, callable $callback){
-
+        $this->events[$type][] = $callback;
     }
 
     public function fireEvent($type){
-
-    }
-    
-    public function go(){
-        echo "print";
+        $ev = $this->events[$type];
+        foreach($ev as $callbeck){
+            if(!is_callable($callbeck)){
+                throw new \Exception('Function is not collable in event type name:' . $type);
+            }
+            call_user_func($callbeck);
+        }
     }
 }
